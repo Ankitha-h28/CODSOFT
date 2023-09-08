e9e2aadfb96c84a142976171f814fd1f39e871dd
Ankitha-h28 <144414601+Ankitha-h28@users.noreply.github.com>
2023-09-08 12:07:58 +0530

    Add files via upload

---
+++ CODSOFT/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	    public static void main(String[] args) {
+	        BankAccount userAccount = new BankAccount(5000.0); 
+	        ATM atm = new ATM(userAccount);
+	        atm.run();
+	    }
+	}
+
